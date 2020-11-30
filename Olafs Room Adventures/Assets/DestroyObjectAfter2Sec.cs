5ac023da6f80c8793eceef52fe3f6cb48b6725e0
neerwright <neerwright@gmail.com>
2020-11-30 20:01:34 +0100

    Radio

---
+++ Olafs Room Adventures/Assets/DestroyObjectAfter2Sec.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectAfter2Sec : MonoBehaviour
+{
+    private float InitTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - InitTime > 2) {
+            Destroy(gameObject);
+        }
+    }
+}

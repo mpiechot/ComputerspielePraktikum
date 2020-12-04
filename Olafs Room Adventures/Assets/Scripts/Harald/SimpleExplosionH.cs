27a582e47af8cd95ef504b9186dfd9b8b67b0856
Grapehippo <haraldkangschroeder@gmail.com>
2020-12-04 14:08:21 +0100

    explosion room harald

---
+++ Olafs Room Adventures/Assets/Scripts/Harald/SimpleExplosionH.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleExplosionH : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject player_root;
+    void OnCollisionEnter (Collision coll) {
+        if (coll.collider.CompareTag ("Player")) {
+            player_root.GetComponent<Rigidbody>().AddForce (Random.Range(-100000,100000),Random.Range(-100000,100000),Random.Range(-100000,100000));
+            Explode ();
+            
+        }
+    }
+    void Explode () {
+        GameObject explosion_objects = Instantiate(explosion, transform.position, Quaternion.identity);
+        explosion_objects.GetComponent<ParticleSystem>().Play();
+        Destroy(gameObject);
+    }
+}

258a1edad6f9fe7a079113c8b0b0b69453a9b933
Philipp Baltik <philipp.baltik@student.uni-tuebingen.de>
2020-12-07 13:05:21 +0100

    implementet healthbar
    -added healthbar to canvas
    -no actual damage detection yet
    - press space to take 20 damage for testing

---
+++ Olafs Room Adventures/Assets/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        healthBar.setHealth(currentHealth);
+    }
+}

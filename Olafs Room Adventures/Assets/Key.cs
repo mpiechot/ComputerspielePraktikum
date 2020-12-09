5b355a1fa4516a887abf78f2c337eb1242a392f9
mpiechot <marco.piechotta@gmail.com>
2020-12-09 17:43:06 +0100

    Keys Implemented

---
+++ Olafs Room Adventures/Assets/Key.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent KeyCollectedEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            KeyCollectedEvent?.Invoke();
+            Destroy(gameObject);
+        }
+    }
+}

38ec2d8911f38bd224bd4b4fbe2c2d9ec3c12cc0
mpiechot <marco.piechotta@gmail.com>
2021-01-04 14:19:57 +0100

    DestructableObject Component

---
+++ Olafs Room Adventures/Assets/DestructableObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableObject : MonoBehaviour
+{
+    [Space(45)]
+    [Header("Rigidbodys automatically to them")]
+    [Header("Child-Gameobjects. This Script will add")]
+    [Header("Note: Dont use a Rigidbody-Component on")]
+
+    [SerializeField]
+    [Range(0,20)]
+    private float maxImpulseValue;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision: " + collision.impulse.magnitude);
+        if(collision.impulse.magnitude >= maxImpulseValue)
+        {
+            Break();
+        }
+    }
+
+    private void Break()
+    {
+        GetComponent<Collider>().enabled = false;
+        Destroy(gameObject.GetComponent<Rigidbody>());
+        foreach(Collider collider in GetComponentsInChildren<Collider>())
+        {
+            collider.enabled = true;
+            if(collider.gameObject.GetComponent<Rigidbody>() == null)
+            {
+                collider.gameObject.AddComponent(typeof(Rigidbody));
+            }
+        }
+    }
+}

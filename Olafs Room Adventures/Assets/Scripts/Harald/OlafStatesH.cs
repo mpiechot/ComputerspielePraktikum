aab0ef93cf690561fdd36086f6450e2db79eafaf
grapehippo <haraldkangschroeder@gmail.com>
2021-01-04 12:04:20 +0100

    first attempt of saving state

--- Olafs Room Adventures/Assets/Scripts/Harald/OlafStatesH.cs
+++ Olafs Room Adventures/Assets/Scripts/Harald/OlafStatesH.cs
@@ -1,8 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OlafStatesH : MonoBehaviour
 {
     public bool has_key = false;
+
+    public void Update(){
+        if(Input.GetKeyDown("k")){
+            Debug.Log("save scene name");
+            saveSceneName();
+        }
+
+        if(Input.GetKeyDown("l")){
+            Debug.Log("load scene");
+            loadScene();
+        }
+    }
+
+    public void saveSceneName(){
+        SaveSystem.saveSceneName(SceneManager.GetActiveScene().name);
+    }
+
+    public void loadScene(){
+        string name = SaveSystem.loadSceneName();
+        if(name != null){
+            SceneManager.LoadScene(name);
+        }
+        else{
+            Debug.Log("Wugabuga");
+        }
+    }
+
+
+    public void savePlayer(){
+        SaveSystem.savePlayer(this);
+    }
+
+    public void loadPlayer(){
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        has_key = data.has_key;
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        Debug.Log(position);
+        transform.position = position;
+    }
 }
---
+++ Olafs Room Adventures/Assets/Scripts/Harald/SaveSystem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    
+    public static void saveSceneName(string name){
+        Debug.Log(Application.persistentDataPath);
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/savedscenename.lol";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        SceneName data = new SceneName(name);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static string loadSceneName(){
+        string path = Application.persistentDataPath + "/savedscenename.lol";
+        if(File.Exists(path)){
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            SceneName data = formatter.Deserialize(stream) as SceneName;
+            stream.Close();
+
+            return data.name;
+        }
+        else{
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+    public static void savePlayer(OlafStatesH player){
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/savedscenename.lol";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer (){
+        string path = Application.persistentDataPath + "/player.fun";
+        if(File.Exists(path)){
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else{
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+    [System.Serializable]
+    class SceneName{
+        public string name;
+
+        public SceneName(string name){
+            this.name = name;
+        }
+    }
+
+}

cdcb69fe617567c5a9eeb86d5c57ea10d04b98b6
mpiechot <marco.piechotta@gmail.com>
2021-01-07 15:33:48 +0100

    Checkpoint System + Scene Update

---
+++ Olafs Room Adventures/Assets/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent<int> CheckpointReachedEvent;
+    private bool isReached = false;
+    [SerializeField]
+    private int checkpointID = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CheckpointReachedEvent?.Invoke(checkpointID);
+            isReached = true;
+            //Destroy(gameObject);
+        }
+    }
+}
---
+++ Olafs Room Adventures/Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> checkpoints;
+    public void OnCheckPointReached(int cpNum)
+    {
+        if(cpNum < 0 || cpNum >= checkpoints.Count)
+        {
+            Debug.LogError("Assign a valid Checkpoint Number to all Checkpoints or insert the new Checkpoint to the LevelManager!");
+        }
+
+        checkpoints[cpNum].SetActive(true);
+    }
+}

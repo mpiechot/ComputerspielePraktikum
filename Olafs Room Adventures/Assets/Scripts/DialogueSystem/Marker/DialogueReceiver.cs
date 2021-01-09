00a63f17c87ab87da1dda1156459d88eaeff82d5
prep0ster0us <leonie-moedl@web.de>
2021-01-09 10:37:46 +0100

    Introduced Timeline Feature to implement Cutscenes. Extended Dialogue System for Timeline. Implemented intro cutscene for tutorial. Tutorial still unfinished.

---
+++ Olafs Room Adventures/Assets/Scripts/DialogueSystem/Marker/DialogueReceiver.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class DialogueReceiver : MonoBehaviour, INotificationReceiver
+{
+
+    [SerializeField] private DialogueTrigger dialogueTrigger;
+
+    public void OnNotify(Playable origin, INotification notification, object context)
+    {
+        if (notification is DialogueMarker dialogueMarker)
+        {
+            //dialogueTrigger.TriggerDialogue();
+            //Debug.Log(dialogueMarker.Message);
+            dialogueTrigger.dialogue.sentences = new string[1];
+            dialogueTrigger.dialogue.sentences[0] = dialogueMarker.Message;
+            dialogueTrigger.letterDelay = dialogueMarker.Delay;
+            dialogueTrigger.TriggerDialogue();
+        }
+        
+    }
+
+}

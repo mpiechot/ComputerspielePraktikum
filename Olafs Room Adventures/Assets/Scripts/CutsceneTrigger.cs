74221f1dd04b2cb54b0908eff384cfc3049a761f
prep0ster0us <leonie-moedl@web.de>
2021-01-14 11:53:24 +0100

    First room of tutorial. Unfinished

---
+++ Olafs Room Adventures/Assets/Scripts/CutsceneTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneTrigger : MonoBehaviour
+{
+    private bool played = false;
+    private PlayableDirector timeline;
+
+    void Start() 
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !played)
+        {
+            timeline.Play();
+            played = true;
+        }
+    }
+}
---
+++ Olafs Room Adventures/Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject continueButton;
+
+    [SerializeField]
+    private GameObject gameManager;
+
+    public void DeactivatePlayerInteraction()
+    {
+        continueButton.SetActive(false);
+        gameManager.GetComponent<GravityController>().DeactivateGravity();
+        gameManager.GetComponent<GravityController>().enabled = false;
+    }
+
+    public void ActivatePlayerInteraction()
+    {
+        continueButton.SetActive(true);
+        gameManager.GetComponent<GravityController>().enabled = true;
+    }
+
+}

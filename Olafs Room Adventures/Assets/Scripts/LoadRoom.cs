89db789a0138d39f97f0774f9b636ca7f69fec71
prep0ster0us <leonie-moedl@web.de>
2021-01-22 22:55:08 +0100

    Added 2 rooms to level1. Not finished yet.

---
+++ Olafs Room Adventures/Assets/Scripts/LoadRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadRoom : MonoBehaviour
+{
+    public GameObject currentRoom;
+    public GameObject nextRoom;
+    public Transform olaf;
+    public Vector3 startArea;
+
+    public void DestroyRoom()
+    {
+        Destroy(currentRoom);
+        Instantiate(nextRoom);
+        Debug.Log("Room position: " + nextRoom.transform.position);
+        olaf.position = startArea;
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+            DestroyRoom();
+        }
+    }
+}

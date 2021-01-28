aba5be1337ecde566a2f584741250d494ce540e8
neerwright <neerwright@gmail.com>
2021-01-28 18:43:04 +0100

    Pengu Blinzeln

--- Olafs Room Adventures/Assets/Prefabs/Rene/Pengu/Pengu.cs
+++ Olafs Room Adventures/Assets/Prefabs/Rene/Pengu/Pengu.cs
@@ -7,18 +7,28 @@ public class Pengu : MonoBehaviour
     Transform[] ArmsAndFeet= new Transform[4];
     bool moveFeet = false;
 
+
+
     private Animator animator;
     string stateName = "PenguFeet";
 
-
+    public Material m_Material;
+    public Texture penguTex;
+    public Texture penguTexBlinzeln;
     
+
     private AudioSource PenguSource;
     public  AudioClip PenguSound;
     public float Volume = 0.5f;
-
+    private bool CR_Running = false;
     // Start is called before the first frame update
     void Start()
     {
+        
+
+       // m_Material = GetComponent<Renderer>().material;
+        //m_Material.SetTexture("_MainTex", penguTexBlinzeln);
+
         animator = GetComponent<Animator>();
 
         ArmsAndFeet[0] = transform.Find("Arm1");
@@ -31,11 +41,19 @@ public class Pengu : MonoBehaviour
         PenguSource.playOnAwake = false;
         PenguSource.dopplerLevel = 1;
         PenguSource.volume = Volume;
+
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!CR_Running)
+        {
+           StartCoroutine(blinzeln());
+        }
+
         transform.Rotate(1.5f * Time.deltaTime, 1.5f * Time.deltaTime, 0.0f, Space.Self);
 
         if (Input.anyKey)
@@ -63,5 +81,16 @@ public class Pengu : MonoBehaviour
                 PenguSource.Play();
             }
         }
+    }
+
+    IEnumerator blinzeln()
+    {
+        CR_Running = true;
+        
+        m_Material.mainTexture = penguTex;
+        yield return new WaitForSeconds(4);
+        m_Material.mainTexture = penguTexBlinzeln;
+        yield return new WaitForSeconds(1);
+        CR_Running = false;
     }
 }

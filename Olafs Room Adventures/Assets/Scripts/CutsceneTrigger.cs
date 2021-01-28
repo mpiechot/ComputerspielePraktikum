92e446fa7c0f3af6ff91613561b77259940116a3
prep0ster0us <leonie-moedl@web.de>
2021-01-28 22:10:37 +0100

    Level 1 Room 1 almost finished. Transit to next room missing.

--- Olafs Room Adventures/Assets/Scripts/CutsceneTrigger.cs
+++ Olafs Room Adventures/Assets/Scripts/CutsceneTrigger.cs
@@ -3,16 +3,31 @@ using UnityEngine.Playables;
 
 public class CutsceneTrigger : MonoBehaviour
 {
+    public bool passCollider;
+
     private bool played = false;
     [SerializeField]
     private PlayableDirector timeline;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player" && !played)
+        if (!passCollider && other.tag == "Player" && !played)
         {
-            timeline.Play();
-            played = true;
+            TriggerCutscene();
         }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if (passCollider & other.tag == "Player" && !played) 
+        {
+            TriggerCutscene();
+        }
+    }
+
+    private void TriggerCutscene() 
+    {
+        timeline.Play();
+            played = true;
     }
 }

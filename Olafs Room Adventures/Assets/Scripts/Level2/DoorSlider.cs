14b6b92c2efd2c652b93433a557227445947d257
mpiechot <marco.piechotta@gmail.com>
2021-02-09 16:10:23 +0100

    Level2 Update
    - Riddle 1 and 2 done
    - Some sort of level design for the room before riddle 3

---
+++ Olafs Room Adventures/Assets/Scripts/Level2/DoorSlider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSlider : MonoBehaviour
+{
+    [SerializeField]
+    private bool open;
+    [SerializeField]
+    private Vector3 openPosition;
+    [SerializeField]
+    private Vector3 closedPosition;
+    [SerializeField]
+    private float openSpeed = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            transform.position = Vector3.Lerp(transform.position, openPosition, openSpeed);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, closedPosition, openSpeed);
+        }
+    }
+    public void Open()
+    {
+        open = true;
+    }
+    public void Close()
+    {
+        open = false;
+    }
+}
---
+++ Olafs Room Adventures/Assets/Scripts/Level2/Switch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent SwitchOnEvent;
+    [SerializeField]
+    private UnityEvent SwitchOffEvent;
+
+    private Animator anim;
+    [SerializeField]
+    private bool state = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    public void SwitchOn()
+    {
+        state = true;
+        anim.SetBool("SwitchOn", state);
+        SwitchOnEvent.Invoke();
+    }
+    public void SwitchOff()
+    {
+        state = false;
+        anim.SetBool("SwitchOn", state);
+        SwitchOffEvent.Invoke();
+    }
+}

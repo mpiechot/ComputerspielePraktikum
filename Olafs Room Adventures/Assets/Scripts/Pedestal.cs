fbac8e9fb080ae87f3e4ead1db09b7bec4e5d0cd
prep0ster0us <leonie-moedl@web.de>
2021-02-11 18:12:30 +0100

    started room3 level1

---
+++ Olafs Room Adventures/Assets/Scripts/Pedestal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pedestal : MonoBehaviour
+{
+
+    public int id;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PedestalObject") 
+        {
+            int other_id = other.GetComponent<PedestalObject>().id;
+            if (other_id != null)
+            {
+                if (other_id == this.id) 
+                {
+                    other.transform.localPosition = new Vector3(0.8f, 2.7f, -0.95f);
+                    other.transform.eulerAngles = Vector3.zero;
+                    other.GetComponent<Rigidbody>().useGravity = false;
+                    other.GetComponent<Rigidbody>().isKinematic = false;
+                }
+            }
+        }
+    }
+
+}

001d204980f6d2210abc85fb500e0c5d6b0af7ca
grapehippo <haraldkangschroeder@gmail.com>
2021-02-12 10:11:44 +0100

    trying to move data from the defect local Repo to the working repo..

---
+++ Olafs Room Adventures/Assets/Scripts/Harald/DoorOpener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+    public GameObject olaf;
+    public GameObject[] doors;
+    public bool[] keys_collected;
+
+    public float distance_threshold;
+    private bool[] doors_state; //false = close
+
+    void Start(){
+        doors_state = new bool[doors.Length];
+    }
+
+    void Update(){
+        for(int i = 0; i < doors.Length; i++){
+            //if(i == 0) Debug.Log(distance(olaf.transform.position,doors[i].transform.position));
+            if(distance(olaf.transform.position,doors[i].transform.position) < distance_threshold && !doors_state[i] && keys_collected[i]){
+                // prevent reopening
+                doors_state[i] = true;
+
+                // start animation of the respective door
+                Animator anim = doors[i].transform.GetChild(0).GetComponent<Animator>();
+                anim.Play("DoorOpen");
+            }
+        }
+    }
+
+    float distance(Vector3 pos0, Vector3 pos1){
+        return (pos0 - pos1).magnitude;
+    }
+}
---
+++ Olafs Room Adventures/Assets/Scripts/Harald/KeyCollector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollector : MonoBehaviour
+{
+    public int key_id;
+    public GameObject player;
+
+    void OnCollisionEnter (Collision coll) {
+        if (coll.collider.CompareTag ("Player")) {
+            Debug.Log("Key collected " + key_id);
+            player.GetComponent<DoorOpener>().keys_collected[key_id] = true;
+            Destroy(gameObject);
+        }
+    }
+}

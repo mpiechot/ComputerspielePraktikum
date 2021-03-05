322232de892cc705942e48e22083f39c6f8eacf2
neerwright <neerwright@gmail.com>
2021-03-05 14:30:34 +0100

    pengu fixed rotation

--- Olafs Room Adventures/Assets/Prefabs/Rene/Pengu/PenguMovement.cs
+++ Olafs Room Adventures/Assets/Prefabs/Rene/Pengu/PenguMovement.cs
@@ -10,18 +10,26 @@ public class PenguMovement : MonoBehaviour
     //Strauchel Animation
     private Animator animator;
     string stateMoveFeet = "PenguFeet"; //Strauchel Animation beginnt
-
+    string stateMoveForward = "PenguMoveForeward";
+    string stateTurnAround = "turnAround";
+    [SerializeField]
+    private float speed = 4;
+    [SerializeField]
+    private float walkingLength = 4;
     [SerializeField]
     private bool moveFeet = false;
     [SerializeField]
     private bool floatsAround = false;
     [SerializeField]
     private bool walkBackAndForth = false;
+    private bool bMoveForward = false;
 
+    
 
     // Start is called before the first frame update
     void Start()
     {
+       
         getTransformOfLimbs();
         //get animator on Pengu
         animator = GetComponent<Animator>();
@@ -32,6 +40,12 @@ public class PenguMovement : MonoBehaviour
         }
         //playAnimation from the state
         
+        //start moving back and forth
+        if(walkBackAndForth)
+            StartCoroutine(moveBackAndForth(walkingLength));
+        
+
+
     }
 
     // Update is called once per frame
@@ -42,18 +56,49 @@ public class PenguMovement : MonoBehaviour
             animator.Play(stateMoveFeet);
         }
 
+        if (bMoveForward)
+        {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
 
+        
+        
+        transform.Rotate(1.5f * Time.deltaTime, 1.5f * Time.deltaTime, 0.0f, Space.Self);
         if (floatsAround)
         {
             transform.Rotate(1.5f * Time.deltaTime, 1.5f * Time.deltaTime, 0.0f, Space.Self);
         }
     }
 
-    public void moveForeward()
-    { 
+    
+
+    public IEnumerator moveForward(float seconds)
+    {
+        animator.speed = 1; //to stop the animation with a 0 and resume here with the 1
+        animator.Play(stateMoveForward);
+        bMoveForward = true;
+        yield return new WaitForSeconds(seconds);
+        bMoveForward = false;
+        animator.speed = 0;
         
     }
-        
+
+    private IEnumerator moveBackAndForth(float seconds)
+    {
+        while(walkBackAndForth)
+        { 
+            animator.speed = 1;
+            animator.Play(stateMoveForward);
+            bMoveForward = true;
+            yield return new WaitForSeconds(seconds);
+            bMoveForward = false;
+            //to stop the animation
+            animator.speed = 0;
+
+            //turn around 180degres
+            walkBackAndForth = false;
+        }
+    }
 
 
     private void getTransformOfLimbs()

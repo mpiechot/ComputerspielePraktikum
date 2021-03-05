1d753fd767ec60e2401570c8ad473e89ef86ed17
grapehippo <haraldkangschroeder@gmail.com>
2021-03-05 13:20:56 +0100

    wall fix, key to switch, timeline with animaiton

---
+++ Olafs Room Adventures/Assets/Scripts/Harald/SwitchHandler4_0.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHandler4_0 : MonoBehaviour
+{
+    public GameObject olaf;
+    public float distance_threshold;
+    private bool state = false;
+
+    private void Update() {
+        if(!state && distance(olaf.transform.position,transform.position) < distance_threshold){
+            state = true;
+            //GetComponent<Switch>().SwitchOn();
+            GetComponent<CutsceneTrigger>().TriggerCutscene();
+        }
+    }
+
+    float distance(Vector3 pos0, Vector3 pos1){
+        return (pos0 - pos1).magnitude;
+    }
+}

42c57d8044738dcf3e47bfa24de0152e78bda426
grapehippo <haraldkangschroeder@gmail.com>
2021-03-05 15:15:00 +0100

    sd

--- Olafs Room Adventures/Assets/Scripts/CameraController.cs
+++ Olafs Room Adventures/Assets/Scripts/CameraController.cs
@@ -16,10 +16,11 @@ public class CameraController : MonoBehaviour
     void Update()
     {
 
-        if (deactivated) {
+        if (deactivated)
+        {
             return;
         }
-        
+
         if (Input.GetMouseButton(1))
         {
             cinemachine.m_XAxis.m_InputAxisName = "Mouse X";
@@ -34,8 +35,8 @@ public class CameraController : MonoBehaviour
         }
 
         // positive -> 1, negative -> -1, zero -> 0
-        float zoom_direction = Math.Sign(Input.GetAxis("Mouse ScrollWheel")); 
-        if (zoom_direction != 0f ) // forward
+        float zoom_direction = Math.Sign(Input.GetAxis("Mouse ScrollWheel"));
+        if (zoom_direction != 0f) // forward
         {
             cinemachine.m_Orbits[0].m_Height -= zoom_direction * zoom_speed;
             cinemachine.m_Orbits[1].m_Radius -= zoom_direction * zoom_speed;
@@ -44,17 +45,20 @@ public class CameraController : MonoBehaviour
 
     }
 
-    public void shakeCamera(float amplitude,float duration){
-        StartCoroutine(startShake(amplitude,duration));
+    public void shakeCamera(float amplitude, float duration)
+    {
+        StartCoroutine(startShake(amplitude, duration));
     }
 
-    IEnumerator startShake(float amplitude,float duration){
+    IEnumerator startShake(float amplitude, float duration)
+    {
         cinemachine.GetRig(0).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
         cinemachine.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
         cinemachine.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
-        for(int i = 0; i < 10; i++){
-            yield return new WaitForSeconds(duration/10);
-            amplitude = amplitude - amplitude/10;
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(duration / 10);
+            amplitude = amplitude - amplitude / 10;
             cinemachine.GetRig(0).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
             cinemachine.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
             cinemachine.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
@@ -65,5 +69,5 @@ public class CameraController : MonoBehaviour
         cinemachine.GetRig(2).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
     }
 
-    
+
 }

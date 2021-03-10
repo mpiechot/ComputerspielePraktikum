0b2203d16f4d20b210ed906c1f3213fdab7e79c2
prep0ster0us <leonie-moedl@web.de>
2021-03-10 12:32:43 +0100

    fixed the camera issue in room2

---
+++ Olafs Room Adventures/Assets/Scripts/CameraSystem/CameraSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject oldCam;
+    public GameObject newCam;
+
+    public void SwitchCameras()
+    {
+        Debug.Log("Switch camera was called");
+        SetPriority(oldCam, 1);
+        SetPriority(newCam, 20);
+    }
+
+    private void SetPriority(GameObject cam, int prio) 
+    {
+        CinemachineVirtualCamera cvc = cam.GetComponent<CinemachineVirtualCamera>();
+        CinemachineFreeLook cfl = cam.GetComponent<CinemachineFreeLook>();
+
+        if (cvc == null && cfl != null)
+        {
+            cfl.Priority = prio;
+            return;
+        }
+        else if (cfl == null && cvc != null)
+        {
+            cvc.Priority = prio;
+            return;
+        }
+        else
+        {
+            Debug.Log("error: cam has both components (virtual cam and free look cam)");
+        }
+    }
+}

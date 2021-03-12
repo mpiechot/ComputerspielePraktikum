0d1c7f5f8cf0cb0e13165f0456b8c972d60bc743
neerwright <neerwright@gmail.com>
2021-03-12 19:34:40 +0100

    SideCam before elevator

---
+++ Olafs Room Adventures/Assets/ReneStartTimeline.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+
+public class ReneStartTimeline : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    public CinemachineFreeLook FreeLook;
+
+    private bool CR = false;
+    [SerializeField]
+    private bool bStart = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (bStart)
+                timeline.Play();
+
+            if (!bStart)
+                timeline.Stop();
+
+        }
+    }
+
+    
+}

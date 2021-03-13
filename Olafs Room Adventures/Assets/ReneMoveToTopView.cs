4cd7a2f6b522f093834f2a04c572587e360d9660
neerwright <neerwright@gmail.com>
2021-03-13 18:20:18 +0100

    switch to top view cam script

--- Olafs Room Adventures/Assets/ReneMoveToTopView.cs
+++ Olafs Room Adventures/Assets/ReneMoveToTopView.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ReneMoveToTopView : MonoBehaviour
 {
+    private Animator animator;
+
+    private bool FreelookCam = true;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+            if(FreelookCam)
+            {
+                animator.Play("TopViewCam");
+                
+            }
+            else
+            {
+                animator.Play("FreelookCam");
+                
+            }
+            FreelookCam = !FreelookCam;
+        }
     }
 }

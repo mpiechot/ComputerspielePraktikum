d4097291d60e22754878e5e6ba1ab88c79b63cd6
neerwright <neerwright@gmail.com>
2021-03-14 14:35:25 +0100

    trailer sound

---
+++ Olafs Room Adventures/Assets/ReneStartCatLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReneStartCatLevel : MonoBehaviour
+{
+    private AudioSource SoundSource;
+    public AudioClip TrailerSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        SoundSource = gameObject.AddComponent<AudioSource>();
+        SoundSource.clip = TrailerSound;
+        SoundSource.playOnAwake = false;
+
+        SoundSource.Play();
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

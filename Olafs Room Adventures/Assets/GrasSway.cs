fd8fbe7d8fe6a1e8e24f9f2696563b33a2957f6f
mpiechot <marco.piechotta@gmail.com>
2021-03-25 19:12:31 +0100

    Butterfly Fix + LevelDesign + Cleanup

---
+++ Olafs Room Adventures/Assets/GrasSway.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrasSway : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+
+    [SerializeField]
+    private float timeSpeed = 1.0f;
+
+    [SerializeField]
+    private float offset = 0.0f;
+
+    [SerializeField]
+    private float amplitude = 1.0f;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        float currentTime = Time.time * timeSpeed + initialPosition.x + offset;
+
+        //transform.position = initialPosition + Vector3.right * Mathf.Sin(currentTime1) * amplitude1 + Vector3.up * Mathf.Sin(currentTime2) * amplitude2;
+        transform.rotation = initialRotation * Quaternion.Euler(
+            Mathf.Sin(currentTime) * amplitude,
+            0,
+            Mathf.Sin(currentTime) * amplitude);
+    }
+}

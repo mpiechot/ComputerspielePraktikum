b469f3eed64185601deb6975a3a32493ef3854dd
neerwright <neerwright@gmail.com>
2021-03-27 10:04:39 +0100

    Timeline2

---
+++ Olafs Room Adventures/Assets/ReneTeaser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ReneTeaser : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera cineCam;
+    [SerializeField]
+    private Transform EndState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(teaser());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator teaser()
+    {
+        yield return new WaitForSeconds(22);
+        float startTime = Time.time;
+        //cineCam.LookAt = transform;
+        while (Time.time - startTime <= 1)
+        { // until one second passed
+            transform.position = Vector3.Lerp(transform.position, EndState.position, (Time.time - startTime)*0.1f); // lerp from A to B in one second
+            yield return 1; // wait for next frame
+        }
+    }
+}

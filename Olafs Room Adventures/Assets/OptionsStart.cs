9f952935afdaf6bf248349684ecaeaec1277b807
Philipp707 <64785477+Philipp707@users.noreply.github.com>
2021-03-28 16:11:25 +0200

    menu update
    - options menu added
    -resolution change and fullscreen should work
    - rest is non functional as of now

---
+++ Olafs Room Adventures/Assets/OptionsStart.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsStart : MonoBehaviour
+{
+    Resolution[] resolutions;
+
+    public Dropdown resolutionsDropdown;
+
+    public GameObject qualitymenu, soundmenu, self, main;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionsIndex = 0;
+        for (int i =0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionsIndex = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionsIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+    }
+
+    public void SetVideo()
+    {
+        soundmenu.SetActive(false);
+        qualitymenu.SetActive(true);
+    }
+
+    public void SetAudio()
+    {
+        qualitymenu.SetActive(false);
+        soundmenu.SetActive(true);
+    }
+
+    public void back()
+    {
+        self.SetActive(false);
+        main.SetActive(true);
+    }
+}
